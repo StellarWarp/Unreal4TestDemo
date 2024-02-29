16d822174937bcf631775e8b1a73d08a4dd4f1e6
StellarWarp <49562703+StellarWarp@users.noreply.github.com>
2024-02-29 12:31:33 +0800

    initial commit

---
+++ Source/DemoProject/MyGameInstance.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMOPROJECT_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	
+};
